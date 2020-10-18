b84418e1e40d1738b43f9ea50393cdab42bd875f
faisalrao778 <57991766+faisalrao778@users.noreply.github.com>
2020-10-18 12:56:01 +0500

    Add files via upload

---
+++ Calculator Challenge/calculator.js
@@ -0,0 +1,33 @@
+
+function calcNumbers(input){
+
+alert(input);
+//var op = document.getElementByClass("numbers").value;
+//var paragraph = document.getElementById("txtMain");
+document.getElementById("txtMain").value = input;
+
+if(op === "+"){
+
+document.getElementByClass('answer').value = n1+n2;
+
+}
+if(op === "-"){
+
+document.getElementByClass('answer').value = n1-n2;
+
+}
+if(op === "*"){
+
+document.getElementByClass('answer').value = n1*n2;
+
+}
+if(op === "/"){
+
+document.getElementByClass('answer').value = n1/n2;
+
+}
+
+
+
+
+}
